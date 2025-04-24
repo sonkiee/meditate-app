cec4fe5cd4bd3d64572533b3f5c927beb49447a1
sonkiee <annagu.kennedy@gmail.com>
2025-04-24 10:23:50 +0000

    Apr 24, 2025, 11:23 AM

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import "react-native-gesture-handler";
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
