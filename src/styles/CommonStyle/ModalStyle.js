9eb763ec26c284ca92632c8e5c711768b7bfcf11
sonkiee <annagu.kennedy@gmail.com>
2025-04-24 11:10:45 +0000

    Apr 24, 2025, 12:10 PM

--- src/styles/CommonStyle/ModalStyle.js
+++ src/styles/CommonStyle/ModalStyle.js
@@ -1,40 +1,42 @@
-import { StyleSheet } from 'react-native';
-import { SF, SH, SW,Fonts } from '../../utils';
+import { StyleSheet } from "react-native";
+import { SF, SH, SW } from "../../utils";
+import { Fonts } from "../../utils/Fonts";
 
-export default customStyles = (Colors) => StyleSheet.create({
-  SpContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  closeView: {
-    width: '100%'
-  },
-  SptextStyle: {
-    color: Colors.wageningen_green,
-    fontSize: SF(45),
-    fontFamily: Fonts.Poppins_Medium
-  },
-  modalcenteredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: SH(17),
-    position: 'relative',
-    backgroundColor: Colors.philippine_silver,
-    width: '100%'
-  },
-  modalView: {
-    width: '100%',
-    borderRadius: SH(15),
-    paddingVertical: SH(20),
-    paddingHorizontal: SW(20),
-    alignItems: "center",
-  },
-  modalbuttonClose: {
-    alignItems: "flex-end",
-    position: 'absolute',
-    right: SW(20),
-    top: SH(20)
-  },
-});
+export default customStyles = (Colors) =>
+  StyleSheet.create({
+    SpContainer: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    closeView: {
+      width: "100%",
+    },
+    SptextStyle: {
+      color: Colors.wageningen_green,
+      fontSize: SF(45),
+      fontFamily: Fonts.Poppins_Medium,
+    },
+    modalcenteredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: SH(17),
+      position: "relative",
+      backgroundColor: Colors.philippine_silver,
+      width: "100%",
+    },
+    modalView: {
+      width: "100%",
+      borderRadius: SH(15),
+      paddingVertical: SH(20),
+      paddingHorizontal: SW(20),
+      alignItems: "center",
+    },
+    modalbuttonClose: {
+      alignItems: "flex-end",
+      position: "absolute",
+      right: SW(20),
+      top: SH(20),
+    },
+  });
