5726b4b27e25446a1b1d4d5c57cd68a11c07ee74
sonkiee <annagu.kennedy@gmail.com>
2025-04-24 11:17:57 +0000

    Apr 24, 2025, 12:17 PM

--- src/components/commonComponents/BottomTabMenu.js
+++ src/components/commonComponents/BottomTabMenu.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import BottomTab from './BottomTab';
-import { Fonts, SF } from '../../utils';
-import { RouteName } from '../../routes';
-import { useTheme } from '@react-navigation/native';
-import { VectoreIcons } from '../../components';
+import React from "react";
+import BottomTab from "./BottomTab";
+import { SF } from "../../utils";
+import { RouteName } from "../../routes";
+import { useTheme } from "@react-navigation/native";
+import { VectoreIcons } from "../../components";
+import { Fonts } from "../../utils/Fonts";
 
 function BottomTabMenu(props) {
   const { Colors } = useTheme();
@@ -11,50 +12,118 @@ function BottomTabMenu(props) {
   const { navigation, selected } = props;
   const tabs = [
     {
-      id: '1',
-      name: '',
-      activeIcon: <VectoreIcons icon="MaterialCommunityIcons" name="home" color={Colors.white} size={SF(25)} />,
-      inactiveIcon: <VectoreIcons icon="MaterialCommunityIcons" name="home" color={Colors.white} size={SF(25)} />
+      id: "1",
+      name: "",
+      activeIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="home"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
+      inactiveIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="home"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
     },
     {
-      id: '2',
-      name: '',
-      activeIcon: <VectoreIcons icon="MaterialCommunityIcons" name="yoga" color={Colors.white} size={SF(25)} />,
-      inactiveIcon: <VectoreIcons icon="MaterialCommunityIcons" name="yoga" color={Colors.white} size={SF(25)} />
+      id: "2",
+      name: "",
+      activeIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="yoga"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
+      inactiveIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="yoga"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
     },
     {
-      id: '3',
-      name: '',
-      activeIcon: <VectoreIcons icon="MaterialCommunityIcons" name="flower-tulip" color={Colors.white} size={SF(25)} />,
-      inactiveIcon: <VectoreIcons icon="MaterialCommunityIcons" name="flower-tulip" color={Colors.white} size={SF(25)} />
+      id: "3",
+      name: "",
+      activeIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="flower-tulip"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
+      inactiveIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="flower-tulip"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
     },
     {
-      id: '4',
-      name: '',
-      activeIcon: <VectoreIcons icon="MaterialCommunityIcons" name="shape" color={Colors.white} size={SF(25)} />,
-      inactiveIcon: <VectoreIcons icon="MaterialCommunityIcons" name="shape" color={Colors.white} size={SF(25)} />
+      id: "4",
+      name: "",
+      activeIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="shape"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
+      inactiveIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="shape"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
     },
     {
-      id: '5',
-      name: '',
-      activeIcon: <VectoreIcons icon="MaterialCommunityIcons" name="account" color={Colors.white} size={SF(25)} />,
-      inactiveIcon: <VectoreIcons icon="MaterialCommunityIcons" name="account" color={Colors.white} size={SF(25)} />
+      id: "5",
+      name: "",
+      activeIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="account"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
+      inactiveIcon: (
+        <VectoreIcons
+          icon="MaterialCommunityIcons"
+          name="account"
+          color={Colors.white}
+          size={SF(25)}
+        />
+      ),
     },
-
   ];
   onPressHandle = (id) => {
     // console.log(id)
-    id == 1 ?
-      navigation.replace(RouteName.HOME_SCREEN)
-      : id == 2 ?
-        navigation.navigate(RouteName.YOGA_SCREEN)
-        : id == 3 ?
-          navigation.navigate(RouteName.MEDITATION_SCREEN)
-          : id == 4 ?
-            navigation.navigate(RouteName.CATEGORY_SCREEN)
-            : navigation.navigate(RouteName.EDIT_PROFILE_SCREEN)
-
-  }
+    id == 1
+      ? navigation.replace(RouteName.HOME_SCREEN)
+      : id == 2
+      ? navigation.navigate(RouteName.YOGA_SCREEN)
+      : id == 3
+      ? navigation.navigate(RouteName.MEDITATION_SCREEN)
+      : id == 4
+      ? navigation.navigate(RouteName.CATEGORY_SCREEN)
+      : navigation.navigate(RouteName.EDIT_PROFILE_SCREEN);
+  };
   return (
     <>
       <BottomTab
@@ -62,7 +131,11 @@ function BottomTabMenu(props) {
         tabBarContainerBackground={Colors.theme_backgound_second}
         tabBarBackground={Colors.white}
         activeTabBackground={Colors.theme_backgound_second}
-        labelStyle={{ color: Colors.white, fontSize: SF(10), fontFamily: Fonts.RobotoCondensed_Bold }}
+        labelStyle={{
+          color: Colors.white,
+          fontSize: SF(10),
+          fontFamily: Fonts.RobotoCondensed_Bold,
+        }}
         onTabChange={(e) => onPressHandle(e.id)}
         defaultActiveTabIndex={selected}
       />
--- src/styles/SweetAlertModalStyle.js
+++ src/styles/SweetAlertModalStyle.js
@@ -1,77 +1,79 @@
-import { StyleSheet ,Platform} from 'react-native';
-import { SF, SH,  widthPercent, SW,Fonts } from '../utils';
+import { StyleSheet, Platform } from "react-native";
+import { SF, SH, widthPercent, SW } from "../utils";
+import { Fonts } from "../utils/Fonts";
 
-export default SweetAlertModalStyle = (Colors) => StyleSheet.create({
+export default SweetAlertModalStyle = (Colors) =>
+  StyleSheet.create({
     settext: {
-        color: Colors.theme_backgound,
-        fontSize: 20,
-        paddingLeft: 20,
-        paddingRight: 20,
-        textAlign: 'center',
-        fontFamily:Fonts.RobotoCondensed_Regular
+      color: Colors.theme_backgound,
+      fontSize: 20,
+      paddingLeft: 20,
+      paddingRight: 20,
+      textAlign: "center",
+      fontFamily: Fonts.RobotoCondensed_Regular,
     },
     centeredView: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 22,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22,
     },
     modalView: {
-        backgroundColor: Colors.transpharent,
-        borderRadius: 7,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-        paddingTop: 20,
-        paddingBottom: 20,
-        width: '95%'
+      backgroundColor: Colors.transpharent,
+      borderRadius: 7,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+      paddingTop: 20,
+      paddingBottom: 20,
+      width: "95%",
     },
     setroundcenter: {
-        flexDirection: 'row',
-        justifyContent: 'center'
+      flexDirection: "row",
+      justifyContent: "center",
     },
     checkiconright: {
-        borderWidth: 3,
-        height: 80,
-        width: 80,
-        borderRadius: 100,
-        flexDirection: 'row',
-        alignContent: 'center',
-        justifyContent: 'center'
+      borderWidth: 3,
+      height: 80,
+      width: 80,
+      borderRadius: 100,
+      flexDirection: "row",
+      alignContent: "center",
+      justifyContent: "center",
     },
     setbackgroundicon: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        paddingTop: 15,
+      flexDirection: "row",
+      justifyContent: "center",
+      paddingTop: 15,
     },
     registertextset: {
-        paddingTop: 25,
-        paddingBottom: 0,
-        flexDirection: 'row',
-        justifyContent: 'center',
+      paddingTop: 25,
+      paddingBottom: 0,
+      flexDirection: "row",
+      justifyContent: "center",
     },
     buttonminview: {
-        flexDirection: 'row',
-        justifyContent: "space-around",
-        paddingTop: 20,
+      flexDirection: "row",
+      justifyContent: "space-around",
+      paddingTop: 20,
     },
     setokbutton: {
-        width: '30%'
+      width: "30%",
     },
     setbgcolorgrsay: {
-        backgroundColor: Colors.gray,
-        height: '100%',
-        alignItems: 'center',
-        flexDirection: 'row',
-        opacity: Platform.OS === 'ios' ? 2 : 0.9,
+      backgroundColor: Colors.gray,
+      height: "100%",
+      alignItems: "center",
+      flexDirection: "row",
+      opacity: Platform.OS === "ios" ? 2 : 0.9,
     },
-    loginSuccessIcon:{
-        width:SW(180),
-        alignSelf:'center'
-    }
-});
+    loginSuccessIcon: {
+      width: SW(180),
+      alignSelf: "center",
+    },
+  });
